dd589c3361636db86c204e6dd04e91b38f8d2e50
B4RB4RO554 <mazenkababoulpn@gmail.com>
2024-11-07 18:55:13 +0100

    just loggers  no test

--- src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.kaddem.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.Serializable;
 import java.util.Set;
@@ -9,6 +11,9 @@ import javax.persistence.*;
 
 @Entity
 public class Equipe implements Serializable{
+
+    private static final Logger logger = LogManager.getLogger(Equipe.class);
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer idEquipe;
@@ -23,18 +28,21 @@ public class Equipe implements Serializable{
     @OneToOne
     private DetailEquipe detailEquipe;
 
-    public Equipe() {
+    public Equipe() { logger.info("Default Equipe constructor called");
         // TODO Auto-generated constructor stub
     }
 
     public Equipe(String nomEquipe) {
+
         this.nomEquipe = nomEquipe;
+        logger.info("Creating Equipe with nomEquipe: {} ", nomEquipe);
     }
 
     public Equipe(String nomEquipe, Niveau niveau) {
         super();
         this.nomEquipe = nomEquipe;
         this.niveau = niveau;
+        logger.info("Creating Equipe with nomEquipe: {} and niveau: {}  ", nomEquipe, niveau);
     }
 
     public Equipe(Integer idEquipe, String nomEquipe, Niveau niveau) {
@@ -42,6 +50,7 @@ public class Equipe implements Serializable{
         this.idEquipe = idEquipe;
         this.nomEquipe = nomEquipe;
         this.niveau = niveau;
+        logger.info("Creating Equipe with idEquipe: {} , nomEquipe: {} and niveau: {}  ", idEquipe, nomEquipe, niveau);
     }
 
     public Equipe(String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
@@ -49,6 +58,7 @@ public class Equipe implements Serializable{
         this.niveau = niveau;
         this.etudiants = etudiants;
         this.detailEquipe = detailEquipe;
+        logger.info("Creating Equipe with nomEquipe: {} , niveau: {}, etudiants: {} and detailEquipe: {}  ", nomEquipe, niveau, etudiants, detailEquipe);
     }
 
     public Equipe(Integer idEquipe, String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
@@ -57,41 +67,68 @@ public class Equipe implements Serializable{
         this.niveau = niveau;
         this.etudiants = etudiants;
         this.detailEquipe = detailEquipe;
+        logger.info("Creating Equipe with idEquipe: {}, nomEquipe: {}, niveau: {}, etudiants: {} and detailEquipe: {}  ", idEquipe, nomEquipe, niveau, etudiants, detailEquipe);
     }
 
+
+
+
     public Set<Etudiant> getEtudiants() {
+        logger.debug("Getting etudiant list: {}", etudiants);
         return etudiants;
     }
 
     public void setEtudiants(Set<Etudiant> etudiants) {
         this.etudiants = etudiants;
+        logger.debug("Setting students: {}", etudiants);
     }
 
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+
     public DetailEquipe getDetailEquipe() {
+        logger.debug("Getting etquipe details: {}", detailEquipe);
         return detailEquipe;
     }
 
     public void setDetailEquipe(DetailEquipe detailEquipe) {
         this.detailEquipe = detailEquipe;
+        logger.debug("Setting team details: {}", detailEquipe);
     }
 
+    ////////////////////////////////////////////////////////////////////////
+
     public Integer getIdEquipe() {
+        logger.debug("Getting equipe ID: {}", idEquipe);
         return idEquipe;
     }
     public void setIdEquipe(Integer idEquipe) {
         this.idEquipe = idEquipe;
+        logger.debug("Setting team id: {}", idEquipe);
     }
+
+    //////////////////////////////////////////////////////////////////////////
+
     public String getNomEquipe() {
+        logger.debug("Getting equipe name: {}", nomEquipe);
         return nomEquipe;
     }
     public void setNomEquipe(String nomEquipe) {
         this.nomEquipe = nomEquipe;
+        logger.debug("Setting team name: {}", nomEquipe);
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+
     public Niveau getNiveau() {
+        logger.debug("Getting equipe niveau: {}", niveau);
         return niveau;
     }
     public void setNiveau(Niveau niveau) {
         this.niveau = niveau;
+        logger.debug("Setting team lvl: {}", niveau);
     }
 
 }
